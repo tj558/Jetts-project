8669501202766d8d89444b523ffec3f9cbdfefce
Khalids1066 <khalid.saeed1066@gmail.com>
2016-05-10 11:59:10 +1200

    Changed some stuff

---
+++ javascript/formvalidation.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(form)
+        .formValidation({
+            ... options ...
+        })
+        .on('success.form.fv', function(e) {
+            // Prevent form submission
+            e.preventDefault();
+
+            // Some instances you can use are
+            var $form = $(e.target),        // The form instance
+                fv    = $(e.target).data('formValidation'); // FormValidation instance
+
+            // Do whatever you want here ...
+    });
+});
