52e19ba8aab3f8509e3530926858ac5c35875b9a
tj558 <therwc2011@gmail.com>
2016-08-03 10:24:01 +1200

    new files
    please dont worry about bad naming of variables at the moment will fix
    later

---
+++ app1.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+var database ="random@gmail.com";
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: false })); // for parsing 
+app.use(express.static(__dirname));//to have acces to images for the html
+
+
+app.get('/', function(req, res){
+  res.sendfile('index2.html');
+});
+
+app.post('/signupsubmit', function(req, res){
+	
+	var email=req.body.email;
+	
+	if(email!=database){
+		res.sendfile('medicalForm.html');
+	}else{
+		res.send("error");
+	}
+
+});
+
+app.post('/medicalsubmit', function(req, res){	
+		res.sendfile('pricingForm.html');
+	
+});
+
+http.listen(5000, function(){
+  console.log('listening on version v2 port:5000');
+});
